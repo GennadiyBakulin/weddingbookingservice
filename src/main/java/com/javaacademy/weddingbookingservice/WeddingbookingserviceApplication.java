dcbc253af4e267aa86e45e54e212f1a88085f526
Gennadiy Bakulin <g.bakulin@yandex.ru>
2024-12-07 16:59:11 +0700

    init commit

---
+++ src/main/java/com/javaacademy/weddingbookingservice/WeddingbookingserviceApplication.java
@@ -0,0 +1,13 @@
+package com.javaacademy.weddingbookingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingbookingserviceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WeddingbookingserviceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/javaacademy/weddingbookingservice/WeddingbookingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaacademy.weddingbookingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingbookingserviceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
