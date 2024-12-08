05f8a37feb143ec0f40d8d4e8a1bb4959a4ff9ce
Gennadiy Bakulin <g.bakulin@yandex.ru>
2024-12-08 18:51:28 +0700

    Вынес часть адреса в BookingController в @RequestMapping

--- src/main/java/com/javaacademy/weddingbookingservice/controller/BookingController.java
+++ src/main/java/com/javaacademy/weddingbookingservice/controller/BookingController.java
@@ -12,26 +12,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/booking")
 @CrossOrigin
 public class BookingController {
 
   private final BookingService bookingService;
 
-  @GetMapping("/booking/month/{monthNumber}")
-  public List<BookingDto> getBookingForNumber(@PathVariable int monthNumber) {
-    return bookingService.getBookingForNumber(monthNumber);
-  }
-
-  @PostMapping("/booking")
+  @PostMapping
   public void saveBooking(@RequestBody Booking booking) {
     bookingService.saveBooking(booking);
   }
 
-  @GetMapping("/booking/month/{monthNumber}/free")
+  @GetMapping("/month/{monthNumber}")
+  public List<BookingDto> getBookingForNumber(@PathVariable int monthNumber) {
+    return bookingService.getBookingForNumber(monthNumber);
+  }
+
+  @GetMapping("/month/{monthNumber}/free")
   public Map<String, Integer> getCountBookingDayOfMonth(@PathVariable int monthNumber) {
     Map<String, Integer> map = new HashMap<>();
     map.put("count", bookingService.getCountBookingDayOfMonth(monthNumber));
